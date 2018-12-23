72a01ac1df05952255f14c50f3ffca221f9b734b
Rajiv K Ramakrishnan <krajiv@athenahealth.com>
2018-12-23 18:40:40 +0530

    adding prometheus

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-require('http')
-  .createServer((req, res) => res.end('ok'))
-  .listen(8000, () => {
-    console.log('Starting app on 8000');
-  });
+import express from 'express';
+import { attach } from './helpers/appmetrics-prometheus';
+
+const app = express();
+
+/**
+ * Get port from environment.
+ */
+const port = process.env.MAXIMUS_PORT || 5001;
+app.set('port', port);
+
+app.get('*', (req, res) => {
+  res.send('app loaded');
+});
+attach();
+
+app.listen(port || config.port, () => {
+  console.log(`App started in port ${port}`);
+});
